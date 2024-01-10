09a950c07e297b1c9905057503bbf46017e212a4
Znak <olena,znak@refinitiv.com>
2024-01-10 22:13:26 +0200

    Amazon syndicate task02

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.LambdaUrlConfig;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "hello_world",
+	roleName = "hello_world-role",
+	isPublishVersion = true
+)
+@LambdaUrlConfig(
+	authType = AuthType.NONE,
+	invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+
+		return new APIGatewayProxyResponseEvent()
+			.withStatusCode(200)
+			.withBody("Hello from Lambda");
+	}
+}
